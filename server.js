8b0bef5e2c26f34fa9bebb8857952c192fa16f26
John Climie <johnclimie00@gmail.com>
2022-01-21 11:49:50 -0500

    Changed location of server.js and notes.html works as it should

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-})
-
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'notes.html'));
-})
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`)

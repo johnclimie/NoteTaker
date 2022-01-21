20c565bb2c20fa262cbae8a5df6111bfbd9e5fc3
John Climie <johnclimie00@gmail.com>
2022-01-21 12:06:57 -0500

    Added API Routes

--- server.js
+++ server.js
@@ -17,6 +17,14 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+app.get('/api/notes', (req, res) => {
+
+})
+
+app.post('/api/notes', (req, res) => {
+
+})
+
 app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`)
 })

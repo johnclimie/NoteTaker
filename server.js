b68ae1441bd7dd2e22a2c04287cb2ed3d25b5a78
John Climie <johnclimie00@gmail.com>
2022-01-22 16:00:46 -0500

    Added api/notes route

--- server.js
+++ server.js
@@ -2,34 +2,36 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const util = require('util');
-const notes = require('./db/db.json')
+const db = require('./db/db.json')
 
 const PORT = 3001;
 
 const app = express();
-const readFromFile = util.promisify(fs.readFile);
 
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-});
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
 app.get('/api/notes', (req, res) => {
-    
+    res.sendFile(path.join(__dirname, '/db/db.json'));
 })
 
-app.post('/api/notes', (req, res) => {
-
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
 })
+
+const readFromFile = util.promisify(fs.readFile);
+
+// app.get('/api/notes', (req, res) => {
+//     res.sendFile(path.join(__dirname, db))
+// })
+
 
 app.listen(PORT, () => {
-    console.log(`Example app listening at http://localhost:${PORT}`)
+    console.log(`App listening at http://localhost:${PORT}`)
 })

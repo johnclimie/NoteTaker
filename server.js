9b954642efe80e684ff11b105d4530290571c9e5
John Climie <johnclimie00@gmail.com>
2022-01-23 12:14:48 -0500

    Added UUID

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const util = require('util');
-const db = require('./db/db.json')
+const uuid = require('uuid');
 
 const PORT = 3001;
 
@@ -17,20 +16,13 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
-app.get('/api/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/db/db.json'));
-})
-
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
 })
 
-const readFromFile = util.promisify(fs.readFile);
-
-// app.get('/api/notes', (req, res) => {
-//     res.sendFile(path.join(__dirname, db))
-// })
-
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/db/db.json'))
+})
 
 app.listen(PORT, () => {
     console.log(`App listening at http://localhost:${PORT}`)
